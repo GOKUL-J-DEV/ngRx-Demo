9b0837182d131b23f6fa4f714c361ae88b32b75d
GOKUL-J-DEV <gokul@sparkouttech.com>
2025-05-20 19:58:11 +0530

    global state management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    importProvidersFrom([StoreModule.forRoot({ count : counterReducer})]),
+]
 };
